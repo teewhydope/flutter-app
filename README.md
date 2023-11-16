1f56c545a3bdea7385d7cfbd1cb835bed9b358e2
teewhydope <ibrahimanimasahun@gmail.com>
2023-11-16 13:28:35 +0100

    update readme, minor fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flutter-app
+
+Build folder has been cleaned, run flutter pub get to re-generate the build folder
--- lib/ui/screen/address/address_screen.dart
+++ lib/ui/screen/address/address_screen.dart
@@ -52,109 +52,113 @@ class _AddressScreenState extends State<AddressScreen> {
         child: Form(
           key: formKey,
           child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: Column(
-                children: [
-                  SizedBox(height: 40.h),
-                  const TextView(
-                    "Please enter your information as written on your ID document",
-                    fontSize: 16,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  SizedBox(height: 28.h),
-                  BlocBuilder<CountryCubit, CountryState>(
-                    builder: (context, blocState) {
-                      var countryList =
-                          blocState.countryListResult?.countries ?? [];
-                      return FormField(
-                        builder: (FormFieldState state) {
-                          return InputDecorator(
-                            decoration: const InputDecoration(),
-                            isEmpty: blocState.selectedCountry == '',
-                            child: DropdownButtonHideUnderline(
-                              child: DropdownButton(
-                                value: blocState.selectedCountry,
-                                isDense: true,
-                                onChanged: (value) {
-                                  _countryCubit
-                                      .onSelectedCountryChanged(value!);
-                                },
-                                items: countryList.map((e) {
-                                  //Flag link not working, so removed from code
-                                  return DropdownMenuItem(
-                                    value: e.name,
-                                    child: SizedBox(
-                                      width: .8.sw,
-                                      child: TextView(e.name ?? ""),
-                                    ),
-                                  );
-                                }).toList(),
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
-                  CTextFormField(
-                    controller: prefectureController,
-                    hintText: "Prefecture",
-                  ),
-                  CTextFormField(
-                    controller: municipalityController,
-                    hintText: "Municipality",
-                  ),
-                  CTextFormField(
-                    controller: streetAddressController,
-                    hintText: "Street Address (subarea - block - house ...",
-                    validator: (value) {
-                      //Not clear enough information, example format would have been helpful
-                    },
-                  ),
-                  CTextFormField(
-                    controller: apartmentController,
-                    hintText: "Apartment, suite or unit",
-                  ),
-                  const Spacer(),
-                  BlocBuilder<CountryCubit, CountryState>(
-                    builder: (context, state) {
-                      return SizedBox(
-                        height: 50.h,
-                        width: 1.sw,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            if (formKey.currentState!.validate()) {
-                              RegisteredAdressUiModel(
-                                country: state.selectedCountry,
-                                prefecture: prefectureController.text,
-                                municipality: municipalityController.text,
-                                streetAddress: streetAddressController.text,
-                                apartment: apartmentController.text,
-                              );
-                            }
-                          },
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(
-                                primaryColor.withOpacity(
-                                    1) //TODO: change color based on form validation,
+            child: SingleChildScrollView(
+              child: Container(
+                height: .9.sh,
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Column(
+                  children: [
+                    SizedBox(height: 40.h),
+                    const TextView(
+                      "Please enter your information as written on your ID document",
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    SizedBox(height: 28.h),
+                    BlocBuilder<CountryCubit, CountryState>(
+                      builder: (context, blocState) {
+                        var countryList =
+                            blocState.countryListResult?.countries ?? [];
+                        return FormField(
+                          builder: (FormFieldState state) {
+                            return InputDecorator(
+                              decoration: const InputDecoration(),
+                              isEmpty: blocState.selectedCountry == '',
+                              child: DropdownButtonHideUnderline(
+                                child: DropdownButton(
+                                  value: blocState.selectedCountry,
+                                  isDense: true,
+                                  onChanged: (value) {
+                                    _countryCubit
+                                        .onSelectedCountryChanged(value!);
+                                  },
+                                  items: countryList.map((e) {
+                                    //Flag link not working, so removed from code
+                                    return DropdownMenuItem(
+                                      value: e.name,
+                                      child: SizedBox(
+                                        width: .8.sw,
+                                        child: TextView(e.name ?? ""),
+                                      ),
+                                    );
+                                  }).toList(),
                                 ),
-                            shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30.r),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    CTextFormField(
+                      controller: prefectureController,
+                      hintText: "Prefecture",
+                    ),
+                    CTextFormField(
+                      controller: municipalityController,
+                      hintText: "Municipality",
+                    ),
+                    CTextFormField(
+                      controller: streetAddressController,
+                      hintText: "Street Address (subarea - block - house ...",
+                      validator: (value) {
+                        //Not clear enough information, example format would have been helpful
+                      },
+                    ),
+                    CTextFormField(
+                      controller: apartmentController,
+                      hintText: "Apartment, suite or unit",
+                    ),
+                    const Spacer(),
+                    BlocBuilder<CountryCubit, CountryState>(
+                      builder: (context, state) {
+                        return SizedBox(
+                          height: 50.h,
+                          width: 1.sw,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (formKey.currentState!.validate()) {
+                                RegisteredAdressUiModel(
+                                  country: state.selectedCountry,
+                                  prefecture: prefectureController.text,
+                                  municipality: municipalityController.text,
+                                  streetAddress: streetAddressController.text,
+                                  apartment: apartmentController.text,
+                                );
+                              }
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  primaryColor.withOpacity(
+                                      1) //TODO: change color based on form validation,
+                                  ),
+                              shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(30.r),
+                                ),
                               ),
                             ),
+                            child: const TextView(
+                              "Next",
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                          child: const TextView(
-                            "Next",
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      );
-                    },
-                  )
-                ],
+                        );
+                      },
+                    ),
+                    SizedBox(height: 40.h),
+                  ],
+                ),
               ),
             ),
           ),
